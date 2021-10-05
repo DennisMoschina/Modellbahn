279bea1c42a4a564c72b9da6ee29f58852601726
Dennis <45356478+DennisMoschina@users.noreply.github.com>
2021-10-05 12:43:42 +0200

    refractored Waldbrand to be able to compile on PIO

--- Waldbrand/src/Waldbrand.cpp
+++ Waldbrand/src/Waldbrand.cpp
@@ -14,65 +14,11 @@ Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, LEDPIN, NEO_GRB + NEO_KH
 //Zeit für das Delay zwischen dem Hochzählen der Helligkeit in der Funtkion waldbrandLicht() in Millisekunden
 int delaytime = 2;
 
-
-void setup() {
-#ifdef debug
-Serial.begin(250000);
-#endif
-
-  pixels.begin();
-}
-
-void loop() {
-  waldbrandLicht();
-}
-
-
-
-void waldbrandLicht() {
-  setPixelBrightness(1, 0, 0, 50, 255);
-
-  int i = 50;
-  while(i<255) {
-    
-#ifdef debug
-Serial.println("---------------------------------------------------");
-Serial.print("Hochzählen ");
-Serial.println(i);
-#endif
-
-    setPixelBrightness(0, 255, 40, 0, i);
-    setPixelBrightness(2, 255, 40, 0, 255+50-i);
-    
-    i++;
-    
-    delay(delaytime);
-    
-    if(i>=255) {
-      while(i>50) {
-        
-#ifdef debug
-Serial.println("---------------------------------------------------");
-Serial.print("Runterzählen ");
-Serial.println(i);
-#endif
-
-        setPixelBrightness(0, 255, 40, 0, i);
-        setPixelBrightness(2, 255, 40, 0, 255+50-i);
-          
-        i = i - 1;
-          
-        delay(delaytime);
-      }    
-    }
-  }  
-}
-
 void setPixelBrightness(int pixel, unsigned long red, unsigned long green, unsigned long blue, unsigned long brightness) {
   unsigned long redBrightness = red * brightness / 255;
   unsigned long greenBrightness = green * brightness / 255;
   unsigned long blueBrightness = blue * brightness / 255;
-  
+
   pixels.setPixelColor(pixel, pixels.Color(redBrightness, greenBrightness, blueBrightness));
 
   pixels.show();
@@ -90,4 +36,58 @@ Serial.println(blueBrightness);
 
 Serial.println();
 #endif
+}
+
+
+void waldbrandLicht() {
+  setPixelBrightness(1, 0, 0, 50, 255);
+
+  int i = 50;
+  while(i<255) {
+
+#ifdef debug
+Serial.println("---------------------------------------------------");
+Serial.print("Hochzählen ");
+Serial.println(i);
+#endif
+
+    setPixelBrightness(0, 255, 40, 0, i);
+    setPixelBrightness(2, 255, 40, 0, 255+50-i);
+
+    i++;
+
+    delay(delaytime);
+
+    if(i>=255) {
+      while(i>50) {
+
+#ifdef debug
+Serial.println("---------------------------------------------------");
+Serial.print("Runterzählen ");
+Serial.println(i);
+#endif
+
+        setPixelBrightness(0, 255, 40, 0, i);
+        setPixelBrightness(2, 255, 40, 0, 255+50-i);
+
+        i = i - 1;
+
+        delay(delaytime);
+      }
+    }
+  }
+}
+
+
+
+void setup() {
+#ifdef debug
+Serial.begin(250000);
+#endif
+
+  pixels.begin();
+}
+
+void loop() {
+  waldbrandLicht();
 }
